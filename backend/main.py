8448627759013878c2df24d9776472ed7b0d9f72
galneryus02 <dzarate736@gmail.com>
2025-11-17 13:07:46 -0500

    main.py

--- backend/main.py
+++ backend/main.py
@@ -4,9 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 origins = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "https://frontend-cyfe.onrender.com"
+    "*"
 ]
 
 app.add_middleware(
