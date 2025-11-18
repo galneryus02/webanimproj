7ebbfc53610adebf76909c91318de2b5689e5072
galneryus02 <dzarate736@gmail.com>
2025-11-18 02:22:36 -0500

    shapes.ts

---
+++ frontend/types/shapes.ts
@@ -0,0 +1,21 @@
+export interface Square {
+  x: number
+  y: number
+  size: number
+  vx: number
+  vy: number
+  gravity: number
+  friction: number
+}
+
+export interface Circle {
+  x: number
+  y: number
+  r: number
+  color: string
+  vx: number
+  vy: number
+  friction: number
+  scaleX: number
+  scaleY: number
+}
